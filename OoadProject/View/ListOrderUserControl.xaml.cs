48f970d70837bd43468dd3c5ebccdba25979b97e
Marcos240 <55844998+Marcos240@users.noreply.github.com>
2020-12-03 07:33:18 +0700

    Management order screen (#7)
    * Management Order Screen
    
    * devTu create ManagementOrderUserControl, ListOrderUserControl, AddOrderScreen, EditOrderScreen
    
    Co-authored-by: Marcos240 <=>

---
+++ OoadProject/View/ListOrderUserControl.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OoadProject.View
+{
+    /// <summary>
+    /// Interaction logic for ListOrderUserControl.xaml
+    /// </summary>
+    public partial class ListOrderUserControl : UserControl
+    {
+        public ListOrderUserControl()
+        {
+            InitializeComponent();
+        }
+
+        private void ScrollViewer_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            ScrollViewer scv = (ScrollViewer)sender;
+            scv.ScrollToVerticalOffset(scv.VerticalOffset - e.Delta);
+            e.Handled = true;
+        }
+
+        private void btnAddOrder_Click(object sender, RoutedEventArgs e)
+        {
+            AddOrderScreen w = new AddOrderScreen();
+            w.ShowDialog();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            EditOrderScreen w = new EditOrderScreen();
+            w.ShowDialog();
+        }
+    }
+}
---
+++ OoadProject/View/ManagementOrderUserControl.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OoadProject.View
+{
+    /// <summary>
+    /// Interaction logic for ManagementOrderUserControl.xaml
+    /// </summary>
+    public partial class ManagementOrderUserControl : UserControl
+    {
+        public ManagementOrderUserControl()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDatHang_GotFocus(object sender, RoutedEventArgs e)
+        {
+            btnDatHang.Background = (Brush)(new BrushConverter()).ConvertFrom("#808080");
+            btnNhapHang.Background = (Brush)(new BrushConverter()).ConvertFrom("#C0C0C0");
+            btnDSNhapHang.Background = (Brush)(new BrushConverter()).ConvertFrom("#C0C0C0");
+        }
+
+        private void btnNhapHang_GotFocus(object sender, RoutedEventArgs e)
+        {
+            btnDatHang.Background = (Brush)(new BrushConverter()).ConvertFrom("#C0C0C0");
+            btnNhapHang.Background = (Brush)(new BrushConverter()).ConvertFrom("#808080");
+            btnDSNhapHang.Background = (Brush)(new BrushConverter()).ConvertFrom("#C0C0C0");
+        }
+
+        private void btnDSNhapHang_GotFocus(object sender, RoutedEventArgs e)
+        {
+            btnDatHang.Background = (Brush)(new BrushConverter()).ConvertFrom("#C0C0C0");
+            btnNhapHang.Background = (Brush)(new BrushConverter()).ConvertFrom("#C0C0C0");
+            btnDSNhapHang.Background = (Brush)(new BrushConverter()).ConvertFrom("#808080");
+        }
+
+        private void btnDatHang_Click(object sender, RoutedEventArgs e)
+        {
+            if (viewControl.Name != "ListOrderUserControl")
+            {
+                viewControl.Content = new ListOrderUserControl();
+                viewControl.Name = "ListOrderUserControl";
+            }
+        }
+
+        private void uscListOrder_Loaded(object sender, RoutedEventArgs e)
+        {
+            viewControl.Name = "ListOrderUserControl";
+            viewControl.Content = new ListOrderUserControl();
+        }
+    }
+}
