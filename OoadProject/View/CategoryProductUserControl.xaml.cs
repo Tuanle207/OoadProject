b7d09ff4e5709f358843e85daadcb128f46599cf
Marcos240 <=>
2020-12-12 00:35:12 +0700

    Done settingUserControl

---
+++ OoadProject/View/CategoryProductUserControl.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OoadProject.View
+{
+    /// <summary>
+    /// Interaction logic for TypeProductUserControl.xaml
+    /// </summary>
+    public partial class CategoryProductUserControl : UserControl
+    {
+        public CategoryProductUserControl()
+        {
+            InitializeComponent();
+        }
+
+        private void ScrollViewer_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            ScrollViewer scv = (ScrollViewer)sender;
+            scv.ScrollToVerticalOffset(scv.VerticalOffset - e.Delta);
+            e.Handled = true;
+        }
+
+        private void btnAddCategoryProduct_Click(object sender, RoutedEventArgs e)
+        {
+            AddCategoryProductWindow w = new AddCategoryProductWindow();
+            w.ShowDialog();
+        }
+    }
+}
---
+++ OoadProject/View/ManufactureUserControl.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OoadProject.View
+{
+    /// <summary>
+    /// Interaction logic for ManufactureUserControl.xaml
+    /// </summary>
+    public partial class ManufactureUserControl : UserControl
+    {
+        public ManufactureUserControl()
+        {
+            InitializeComponent();
+        }
+
+        private void ScrollViewer_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            ScrollViewer scv = (ScrollViewer)sender;
+            scv.ScrollToVerticalOffset(scv.VerticalOffset - e.Delta);
+            e.Handled = true;
+        }
+
+        private void btnAddManufacture_Click(object sender, RoutedEventArgs e)
+        {
+            AddManufactureWindow w = new AddManufactureWindow();
+            w.ShowDialog();
+        }
+    }
+}
